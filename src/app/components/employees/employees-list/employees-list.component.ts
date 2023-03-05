898063042db529164084cd5ee0cc51171b197e74
julesep3 <50899932+julesep3@users.noreply.github.com>
2023-03-05 13:51:51 -0600

    add Employees with display

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.css']
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
 
+  // employees: Employee[] = [
+  //   {
+  //     id: 'A1',
+  //     name: 'John Smith',
+  //     email: 'JSmith@email.com',
+  //     phone: 4053771111,
+  //     salary: 50000,
+  //     department: 'Accounting'
+  //   },
+  //   {
+  //     id: 'A2',
+  //     name: 'Laura Jones',
+  //     email: 'LJones@email.com',
+  //     phone: 4053771112,
+  //     salary: 55000,
+  //     department: 'Sales'
+  //   },
+  //   {
+  //     id: 'A3',
+  //     name: 'Victor Sanchez',
+  //     email: 'VSanchez@email.com',
+  //     phone: 4053771113,
+  //     salary: 45000,
+  //     department: 'Distribution'
+  //   },
+  //   {
+  //     id: 'A4',
+  //     name: 'Lisa Ying',
+  //     email: 'LYing@email.com',
+  //     phone: 4053771113,
+  //     salary: 53000,
+  //     department: 'Human Resources'
+  //   }
+  // ];
+
+  employees?: Employee[] = [];
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 }
