1fa46edaf00524955319abf3428e21c2144d14b8
julesep3 <50899932+julesep3@users.noreply.github.com>
2023-03-06 11:32:13 -0600

    add components for connecting to db

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
@@ -45,8 +46,16 @@ export class EmployeesListComponent implements OnInit {
 
   employees?: Employee[] = [];
 
-  constructor() {
+  constructor(private employeesService: EmployeesService) {
   }
   ngOnInit(): void {
+    this.employeesService.getAllEmployees().subscribe({
+      next: (employees) => {
+        console.log(employees);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+  });
   }
 }
