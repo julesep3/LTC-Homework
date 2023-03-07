dd48bc66209658192a772b42e15d8d9a9e79c073
julesep3 <50899932+julesep3@users.noreply.github.com>
2023-03-07 15:26:19 -0600

    update visuals

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -9,42 +9,42 @@ import { EmployeesService } from 'src/app/services/employees.service';
 })
 export class EmployeesListComponent implements OnInit {
 
-  // employees: Employee[] = [
-  //   {
-  //     id: 'A1',
-  //     name: 'John Smith',
-  //     email: 'JSmith@email.com',
-  //     phone: 4053771111,
-  //     salary: 50000,
-  //     department: 'Accounting'
-  //   },
-  //   {
-  //     id: 'A2',
-  //     name: 'Laura Jones',
-  //     email: 'LJones@email.com',
-  //     phone: 4053771112,
-  //     salary: 55000,
-  //     department: 'Sales'
-  //   },
-  //   {
-  //     id: 'A3',
-  //     name: 'Victor Sanchez',
-  //     email: 'VSanchez@email.com',
-  //     phone: 4053771113,
-  //     salary: 45000,
-  //     department: 'Distribution'
-  //   },
-  //   {
-  //     id: 'A4',
-  //     name: 'Lisa Ying',
-  //     email: 'LYing@email.com',
-  //     phone: 4053771113,
-  //     salary: 53000,
-  //     department: 'Human Resources'
-  //   }
-  // ];
+  employees: Employee[] = [
+    {
+      id: 'A1',
+      name: 'John Smith',
+      email: 'JSmith@email.com',
+      phone: 4053771111,
+      salary: 50000,
+      department: 'Accounting'
+    },
+    {
+      id: 'A2',
+      name: 'Laura Jones',
+      email: 'LJones@email.com',
+      phone: 4053771112,
+      salary: 55000,
+      department: 'Sales'
+    },
+    {
+      id: 'A3',
+      name: 'Victor Sanchez',
+      email: 'VSanchez@email.com',
+      phone: 4053771113,
+      salary: 45000,
+      department: 'Distribution'
+    },
+    {
+      id: 'A4',
+      name: 'Lisa Ying',
+      email: 'LYing@email.com',
+      phone: 4053771113,
+      salary: 53000,
+      department: 'Human Resources'
+    }
+  ];
 
-  employees?: Employee[] = [];
+  // employees?: Employee[] = [];
 
   constructor() { // private employeesService: EmployeesService
   }
