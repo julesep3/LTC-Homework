891956cb54ceccf3541a158ecdbdc917962ef3da
julesep3 <50899932+julesep3@users.noreply.github.com>
2023-03-09 16:38:59 -0600

    add sign up form

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  signupForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router){}
+
+  ngOnInit(): void {
+      this.signupForm = this.fb.group({
+        email: ['', Validators.required],
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+      })
+  }
+
+  onSignup() {
+
+  }
 
 }
